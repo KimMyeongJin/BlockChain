bacec9d0348a19795a6267f9e9df0d2d004f8e79
myeongjinKim <ab9010@naver.com>
2019-05-07 16:06:52 +0900

    Types in Typescript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Nicolas", age = 24, gender = "male";
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi("Nicolas", 26, "male"));
+//# sourceMappingURL=index.js.map
