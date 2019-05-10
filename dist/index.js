cf5e17105c566b157618f829bfa9fb57551db996
myeongjinKim <ab9010@naver.com>
2019-05-10 18:14:27 +0900

    Blockchain Creating a Block

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const lagom = new Human("Lagom", 18, "male");
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-console.log(sayHi(lagom));
+const gensisBlock = new Block(0, "1010101010101", "", "HelloLagom", 123456);
+let blockchain = [gensisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name:string, age:number, gender:string){
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ){
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this. timestamp = timestamp;
   }
 }
 
-const lagom = new Human("Lagom", 18, "male");
+const gensisBlock:Block = new Block(0,"1010101010101","","HelloLagom",123456);
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-}
-  console.log(sayHi(lagom));
+let blockchain: [Block] = [gensisBlock];
+
+console.log(blockchain);
 
     export{};
 
