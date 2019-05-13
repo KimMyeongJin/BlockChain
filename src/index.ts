9634fa14ba474e8e2f2396bf1e27b751a55936ac
myeongjinKim <ab9010@naver.com>
2019-05-13 17:55:53 +0900

    Validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
-  public index: number;
-  public hash: string;
-  public previousHash: string;
-  public data: string;
-  public timestamp: number;
-  
   static calculateBlockHash = (
     index: number, 
     previousHash:string,
@@ -14,6 +8,19 @@ class Block {
      data:string): string => 
      CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
   
+  static validateStructure = (aBlock : Block): boolean => 
+  typeof aBlock.index === "number" && 
+  typeof aBlock.hash === "string" && 
+  typeof aBlock.previousHash === "string" &&
+  typeof aBlock.timestamp === "number" &&
+  typeof aBlock.data === "string";
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
   constructor(
     index: number,
     hash: string,
@@ -29,8 +36,6 @@ class Block {
   }
 }
 
-
-
 const gensisBlock:Block = new Block(0,"1010101010101","","HelloLagom",123456);
 
 let blockchain: Block[] = [gensisBlock];
@@ -39,9 +44,39 @@ console.log(blockchain);
 
 const getBlockchain = () : Block[] => blockchain;
 
-const getLatestBlcok = () : Block => blockchain[blockchain.length -1];
+const getLatestBlock = () : Block => blockchain[blockchain.length -1];
 
 const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
 
-    export{};
+const createNewBlock = (data:string) : Block => {
+  const previousBlock : Block = getLatestBlock();
+  const newIndex : number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash : string = Block.calculateBlockHash(
+    newIndex, 
+    previousBlock.hash, 
+    newTimestamp, 
+    data
+    );
+    const newBlock : Block = new Block(
+      newIndex, 
+      newHash, 
+      previousBlock.hash, 
+      data, 
+      newTimestamp
+      );
+      return newBlock; 
+};
+
+const isBlockValid = (candidateBlock: Block, previousBlock : Block): boolean => {
+  if(!Block.validateStructure(candidateBlock)){
+    return false; 
+  } else if(previousBlock.index + 1 !== candidateBlock.index){
+    return false;
+  } else if(previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if()
+};
+
+export{};
 
